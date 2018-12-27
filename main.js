0c081c19f100640b50dcc2de581aae471374c610
NaveenKumar <catch.nkn@gmail.com>
2018-12-26 23:05:41 -0600

    Initial Commit

---
+++ main.js
@@ -0,0 +1,17 @@
+load.initialize(async function () {
+});
+
+load.action("Action", async function () {
+    
+    load.log(`Hello there! Let's debug`);
+    
+    var a = 5;
+    var b = 10;
+    var sum = 0;
+    sum = a + b;
+    load.log(sum);
+
+ });
+
+load.finalize(async function () {
+});
